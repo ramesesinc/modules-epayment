e71ddab99f8d1e8b3786f5b01328b34a2b8e1f7f
worgflores <worgflores@gmail.com>
2019-02-13 18:27:01 +0800

    added

---
+++ client/rameses-gov-epayment/src/com/rameses/gov/epayment/models/EORRemittanceInitialModel.groovy
@@ -0,0 +1,93 @@
+package com.rameses.gov.epayment.models;
+
+
+import com.rameses.rcp.annotations.*;
+import com.rameses.rcp.common.*;
+import com.rameses.osiris2.client.*;
+import com.rameses.osiris2.common.*;
+import com.rameses.seti2.models.*;
+import java.text.*;
+ 
+/*******************************************************************************
+* This class is used for Rental, Other Fees and Utilities
+********************************************************************************/
+public class EORRemittanceInitialModel  extends CrudFormModel {
+    
+    @Service("OnlinePaymentResolverService")
+    def onlineSvc;
+
+    @Service("EORRemittanceService")
+    def remittanceSvc;
+    
+    def partner;
+    def partnerList;
+    def selectedItem;
+    
+    void afterCreate() {
+        def m = [_schemaname: 'paymentpartner'];
+        m.where = ["1=1"];
+        partnerList = queryService.getList(m);
+    }
+    
+    @PropertyChangeListener
+    def listener = [
+        "partner" : { o->
+            listHandler.reload();
+        } 
+    ];
+    
+    def listHandler = [
+        fetchList: { o->
+            if( !partner ) return [];
+            def m = [_schemaname: 'eor'];
+            m.where = ["partnerid =:partnerid AND remittanceid IS NULL", [partnerid: partner.objid ]];
+            m.orderBy = "tracedate";
+            return queryService.getList(m);
+        },
+        isMultiSelect: {
+            return true;
+        },
+        afterSelectionChange: {
+            entity.amount = listHandler.selectedValue.sum{ it.amount };
+            binding.refresh("amount");
+        }, 
+        onOpenItem: { o, name-> 
+            if ( !o ) return null;
+            return Inv.lookupOpener('eor:open', [ entity: o]); 
+        }           
+    ] as BasicListModel;
+   
+    def selectedPO;
+    def resolveListHandler = [
+        fetchList: { o->
+            return [];
+        },
+        isMultiSelect: {
+            return true;
+        }
+        
+    ] as BasicListModel;
+    
+    
+    public void resolve() { 
+        resolveListHandler.selectedValue?.each{ 
+            onlineSvc.resolve( it ); 
+        } 
+    }
+    
+    public def save() {
+        if( entity.amount <= 0 )
+            throw new Exception("Please run compute amount first");
+        if(entity.amount != listHandler.selectedValue.sum{it.amount})
+            throw new Exception("Please run compute amount first");
+            
+        if(!MsgBox.confirm("You are about to create a remittance for the selected items. Proceed?")) return null;
+        def arr = listHandler.selectedValue;
+        entity.partnerid = partner.objid;
+        entity.items = arr.collect{ [objid: it.objid ] };
+        entity = remittanceSvc.create( entity );
+        return Inv.lookupOpener("eor_remittance:open", [entity: entity ]);
+    }
+    
+    
+}
---
+++ client/rameses-gov-epayment/src/com/rameses/gov/epayment/models/EORRemittanceModel.groovy
@@ -0,0 +1,77 @@
+package com.rameses.gov.epayment.models;
+
+
+import com.rameses.rcp.annotations.*;
+import com.rameses.rcp.common.*;
+import com.rameses.osiris2.client.*;
+import com.rameses.osiris2.common.*;
+import com.rameses.seti2.models.*;
+import java.text.*;
+ 
+/*******************************************************************************
+* This class is used for Rental, Other Fees and Utilities
+********************************************************************************/
+public class EORRemittanceModel extends CrudFormModel {
+    
+    @Service('EORRemittanceService')
+    def svc; 
+    
+    def eorListHandler = [
+        fetchList: { o->
+            def m = [_schemaname: 'eor'];
+            m.findBy = [remittanceid: entity.objid ];
+            return queryService.getList( m );
+        },
+        onOpenItem: {o,col->
+            def op = Inv.lookupOpener("eor:open", [entity: o ]);
+            op.target = "popup";
+            return op;
+        }
+    ] as BasicListModel;
+    
+    def selectedFund;
+    def fundListHandler = [
+        fetchList: { o->
+            def m = [_schemaname: 'eor_remittance_fund'];
+            m.findBy = [remittanceid: entity.objid ];
+            return queryService.getList( m );
+        }
+    ] as BasicListModel;
+
+    def validateFund() { 
+        if ( !selectedFund ) throw new Exception('Please select fund'); 
+        
+        def p = [:]; 
+        p.handler = { o-> 
+            svc.updateValidation([ objid: selectedFund.objid, validation: o ]); 
+            fundListHandler.reload(); 
+        }
+        def op = Inv.lookupOpener('deposit_validation', p );
+        op.target = 'popup';
+        return op;
+    }
+    
+    def assignBankAccount() { 
+        if ( !selectedFund ) throw new Exception('Please select fund'); 
+        
+        def p = [:]; 
+        p.fundid = selectedFund.fund.objid; 
+        p.onselect = { o-> 
+            svc.updateBankAccount([ objid: selectedFund.objid, bankaccount: o ]); 
+            fundListHandler.reload(); 
+        }
+        def op = Inv.lookupOpener('bankaccount:lookup', p );
+        op.target = 'popup';
+        return op;
+        
+    }
+    
+    void post() { 
+        if ( MsgBox.confirm('You are about to post this transaction. Proceed?')) {
+            def res = svc.post( entity ); 
+            if ( res ) entity.putAll( res ); 
+            
+            binding.refresh();
+        }
+    }
+}
