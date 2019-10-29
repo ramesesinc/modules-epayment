497d6379aa3a0253504f6ff5dbcc67d598c52624
elmonazareno <elmonazareno@gmail.com>
2019-10-29 16:09:22 +0800

    updated

--- cloud-server/rameses-epayment-services.jar/sql/paymentorder.sql
+++ cloud-server/rameses-epayment-services.jar/sql/paymentorder.sql
@@ -1,2 +1,5 @@
 [movePaymentOrderToPaid]
 INSERT INTO paymentorder_paid SELECT * FROM paymentorder WHERE objid=$P{paymentrefid}
+
+[movePaymentOrderToCancelled]
+INSERT INTO paymentorder_cancelled SELECT * FROM paymentorder WHERE objid=$P{paymentrefid}
