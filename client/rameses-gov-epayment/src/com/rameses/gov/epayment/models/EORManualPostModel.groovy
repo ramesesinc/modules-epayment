162a3b9e91a1cf4d293a87f825e95bc4ab9893b6
elmonazareno <elmonazareno@gmail.com>
2020-10-16 11:02:50 +0800

    updates: 1) eor email = added UI to view emails and function to resend in case there is error 2) eor = added button to resend email 3) eor manual post = This is still incomplete. Pls. add code to execute in cloud.    This will just log the manually posted transactions from LGU side. You need EPAYMENT MANAGER role for this

---
+++ client/rameses-gov-epayment/src/com/rameses/gov/epayment/models/EORManualPostModel.groovy
@@ -0,0 +1,42 @@
+package com.rameses.gov.epayment.models;
+
+
+import com.rameses.rcp.annotations.*;
+import com.rameses.rcp.common.*;
+import com.rameses.osiris2.client.*;
+import com.rameses.osiris2.common.*;
+import com.rameses.seti2.models.*;
+import java.text.*;
+ 
+/*******************************************************************************
+* This is for executing payment manually if payment partner fails to send
+* reply to the cloud
+********************************************************************************/
+public class EORManualPostModel  {
+
+    @Service("EORManualPostService")
+    def svc;
+    
+    def entity;
+    def mode;
+    boolean editable;
+    
+    void create() {
+        entity = [:];
+        mode = "create"
+        editable = true;
+    }
+    
+    void open() {
+        entity = svc.open([objid: entity.objid]);
+        mode = "open";
+    }
+    
+    def submit() {
+        if(!MsgBox.confirm("You are about to submit this transaction. Please confirm")) return null;
+        svc.post( entity );
+        MsgBox.alert("Transaction successful");
+        return "_close";
+    }
+    
+}
