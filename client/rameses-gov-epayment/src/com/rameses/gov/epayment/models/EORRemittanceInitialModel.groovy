883f53c86f5b4c1d64fe30757632ca9cc3b90aa8
worgflores <worgflores@gmail.com>
2021-01-14 15:51:51 +0800

    updated

--- client/rameses-gov-epayment/src/com/rameses/gov/epayment/models/EORRemittanceInitialModel.groovy
+++ client/rameses-gov-epayment/src/com/rameses/gov/epayment/models/EORRemittanceInitialModel.groovy
@@ -20,6 +20,7 @@ public class EORRemittanceInitialModel  extends CrudFormModel {
     
     void afterCreate() { 
         partnerList = remittanceSvc.getPaymentPartners(); 
+        entity.state = 'DRAFT'; 
     } 
     
     void setPartner( o ) {
@@ -61,27 +62,67 @@ public class EORRemittanceInitialModel  extends CrudFormModel {
             return true;
         },
         fetchList: { o->
-            return [];
+            def m = [:]; 
+            m.putAll( o ); 
+            m._schemaname = 'eor_paymentorder';
+            m.select = "objid, txndate, txntype, txntypename, paidby, paidbyaddress, amount, controlno"; 
+            m.orderBy = "txndate DESC"; 
+            m.node = [id: 'UNRESOLVED']; 
+            return queryService.getList( m ); 
         }
-    ] as DataListModel;
+    ] as PageListModel;
 
     public void resolve() { 
-        resolveListHandler.selectedValue?.each{ 
-            onlineSvc.resolve( it ); 
+        if ( !resolveListHandler.selectedValue ) { 
+            MsgBox.alert("Please select at least 1 item in the list"); 
+            return; 
         } 
+
+        def param = [ items: resolveListHandler.selectedValue ];
+        param.callbackHandler = { o-> 
+            resolveListHandler.doSearch(); 
+        }
+        Modal.show( "eor_paymentorder:resolve", param ); 
+    }
+    
+    public void moveToCancelled() {
+        if ( !resolveListHandler.selectedValue ) { 
+            MsgBox.alert("Please select at least 1 item in the list"); 
+            return; 
+        } 
+
+        def param = [ items: resolveListHandler.selectedValue ];
+        param.callbackHandler = { o-> 
+            resolveListHandler.doSearch(); 
+        }
+        Modal.show( "eor_paymentorder:cancel", param ); 
     }
     
     public def save() {
-        if ( entity.amount <= 0 )
-            throw new Exception("Please run compute amount first");
-        if ( entity.amount != listHandler.selectedValue.sum{it.amount})
-            throw new Exception("Please run compute amount first");
+        if ( !listHandler.selectedValue ) 
+            throw new Exception("Select at least one item from the unremitted receipts");
             
-        if(!MsgBox.confirm("You are about to create a remittance for the selected items. Proceed?")) return null;
+        def remamt = (entity.amount ? entity.amount : 0.0); 
+        if ( remamt <= 0 )
+            throw new Exception("Amount to remit must be greater than zero");
+
+        def selamt = listHandler.selectedValue.sum{( it.amount ? it.amount : 0.0 )} 
+        if ( !selamt ) selamt = 0.0; 
+        if ( remamt != selamt )
+            throw new Exception("Total amount from selection does not matched to the amount to be remitted");
+            
+        if (!MsgBox.confirm("You are about to create a remittance for the selected items. Proceed?")) return null;
+        
         def arr = listHandler.selectedValue;
         entity.partnerid = partner.objid;
-        entity.items = arr.collect{ [objid: it.objid ] };
-        entity = remittanceSvc.create( entity );
-        return Inv.lookupOpener("eor_remittance:open", [entity: entity ]);
+        entity.items = arr.collect{ [objid: it.objid ] }
+        def res = remittanceSvc.create( entity );
+        if ( res ) entity.putAll( res ); 
+        
+        binding.refresh(); 
+        
+        def op = Inv.lookupOpener("eor_remittance:open", [ entity: entity ]); 
+        op.target = "topwindow"; 
+        return op; 
     }
 }
